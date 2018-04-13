d9d31b8b13c64f38aa98cea791c49aae4adc4315
styks007 <artur.kazmierczak91@gmail.com>
2018-04-13 09:47:56 +0200

    dodane zczytywanie kontaktow z książki adresowej

--- app/src/main/java/com/ericsson/followyourfriend/DebugActivity.java
+++ app/src/main/java/com/ericsson/followyourfriend/DebugActivity.java
@@ -28,6 +28,93 @@ public class DebugActivity extends AppCompatActivity {
 
         // Here, thisActivity is the current activity
         if (ContextCompat.checkSelfPermission(DebugActivity.this,
+                Manifest.permission.READ_PHONE_NUMBERS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(DebugActivity.this,
+                    Manifest.permission.READ_PHONE_NUMBERS)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(DebugActivity.this,
+                        new String[]{Manifest.permission.READ_PHONE_NUMBERS},
+                        1);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(DebugActivity.this,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(DebugActivity.this,
+                    Manifest.permission.READ_CONTACTS)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(DebugActivity.this,
+                        new String[]{Manifest.permission.READ_CONTACTS},
+                        1);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(DebugActivity.this,
+                Manifest.permission.WRITE_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(DebugActivity.this,
+                    Manifest.permission.WRITE_CONTACTS)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(DebugActivity.this,
+                        new String[]{Manifest.permission.WRITE_CONTACTS},
+                        1);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(DebugActivity.this,
                 Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
 
--- app/src/main/java/com/ericsson/followyourfriend/FriendActivity.java
+++ app/src/main/java/com/ericsson/followyourfriend/FriendActivity.java
@@ -1,6 +1,13 @@
 package com.ericsson.followyourfriend;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.Contacts;
+import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +16,8 @@ import android.widget.ScrollView;
 import java.util.ArrayList;
 
 import com.ericsson.Person.Friend;
+
+import static android.provider.ContactsContract.*;
 
 public class FriendActivity extends AppCompatActivity {
 
@@ -33,5 +42,18 @@ public class FriendActivity extends AppCompatActivity {
     public void onClickEdit(View view) {
         Intent intent = new Intent(this,EditFriendActivity.class);
         startActivity(intent);
+    }
+
+    public void onClickImport(View view) {
+        
+        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME+" ASC");
+        while (phones.moveToNext())
+        {
+            String name=phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+            System.out.println(name + "   " + phoneNumber);
+        }
+        phones.close();
     }
 }
