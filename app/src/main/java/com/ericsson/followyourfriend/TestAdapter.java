03eb29d40870ff0acafbad34f6d5e9a169860110
styks007 <artur.kazmierczak91@gmail.com>
2018-04-18 15:58:35 +0200

    dodany obrazek dla statusu przy liscie friendow, usuniety przycisk wstecz

--- app/src/main/java/com/ericsson/followyourfriend/TestAdapter.java
+++ app/src/main/java/com/ericsson/followyourfriend/TestAdapter.java
@@ -1,11 +1,15 @@
 package com.ericsson.followyourfriend;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.telephony.TelephonyManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.ericsson.Person.Friend;
@@ -43,14 +47,36 @@ public class TestAdapter extends ArrayAdapter<Friend> {
     public View getView(int i, View view, ViewGroup viewGroup) {
         View rowView = mInflater.inflate(R.layout.item,viewGroup,false);
 
+        ImageView offline = (ImageView) rowView.findViewById(R.id.offline);
+        ImageView notRegistered = (ImageView) rowView.findViewById(R.id.notRegistered);
+        ImageView online = (ImageView) rowView.findViewById(R.id.online);
         TextView textItem = (TextView) rowView.findViewById(R.id.item_text);
         TextView textItem2 = (TextView) rowView.findViewById(R.id.item_text2);
+
+        Resources res = Resources.getSystem();
 
         Friend friend = (Friend) getItem(i);
 
         if(friend != null) {
             textItem.setText((CharSequence) friend.getmName());
             textItem2.setText(((CharSequence) String.valueOf(friend.getmNumber())));
+            switch (friend.getmStatus()) {
+                case VISIBLE:
+                    online.setVisibility(View.VISIBLE);
+                    offline.setVisibility(View.GONE);
+                    notRegistered.setVisibility(View.GONE);
+                    break;
+                case INVISIBLE:
+                    online.setVisibility(View.GONE);
+                    offline.setVisibility(View.VISIBLE);
+                    notRegistered.setVisibility(View.GONE);
+                    break;
+                case NOT_REGISTERED:
+                    online.setVisibility(View.GONE);
+                    offline.setVisibility(View.GONE);
+                    notRegistered.setVisibility(View.VISIBLE);
+                    break;
+            }
         }
 
         return rowView;
